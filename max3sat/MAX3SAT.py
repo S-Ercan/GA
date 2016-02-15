2537644210c892e001772ff9298680aaa3ec61fe
Selman Ercan <selman@selman-N551JQ>
2016-02-15 20:29:49 +0100

    Started ProblemGenerator implementation.

--- max3sat/MAX3SAT.py
+++ max3sat/MAX3SAT.py
@@ -1,21 +1,21 @@
 from max3sat.Clause import Clause
+from max3sat.Variable import Variable
 
 
 class MAX3SAT:
 
-    def __init__(self):
-        self.variables = []
-        self.clauses = []
+    def __init__(self, variables, clauses):
+        if not isinstance(variables, list):
+            raise TypeError("'variables' must be a list.")
+        if not all(isinstance(v, Variable) for v in variables):
+            raise TypeError("All elements in 'variables' must be Variable instances.")
 
-    def set_variables(self, variables):
-        self.variables = variables
-
-    def set_clauses(self, clauses):
         if not isinstance(clauses, list):
             raise TypeError("'clauses' must be a list.")
         if not all(isinstance(c, Clause) for c in clauses):
             raise TypeError("All elements in 'clauses' must be Clause instances.")
 
+        self.variables = variables
         self.clauses = clauses
 
     def __str__(self):
--- max3sat/ProblemGenerator.py
+++ max3sat/ProblemGenerator.py
@@ -1,4 +1,26 @@
+import random
+
+from max3sat.MAX3SAT import MAX3SAT
+
+
 class ProblemGenerator:
 
     def generate_problem(self):
-        pass
+        variables = self.generate_variables()
+        clauses = self.generate_clauses()
+
+        max3sat = MAX3SAT(variables, clauses)
+
+    def generate_variables(self):
+        variables = []
+        num_variables = random.randint(1, 5)
+        for i in range(num_variables - 1):
+            pass
+        return variables
+
+    def generate_clauses(self):
+        clauses = []
+        num_clauses = random.randint(1, 5)
+        for i in range(num_clauses - 1):
+            pass
+        return clauses
