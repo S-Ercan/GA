15fc0555a4a5b52d32999834d80e343df9309046
Selman Ercan <selman@selman-N551JQ>
2016-02-16 20:26:22 +0100

    Added test cases.

--- sat/tests/test_variable.py
+++ sat/tests/test_variable.py
@@ -1,5 +1,13 @@
 import unittest
 
+from sat.variable import Variable
+
 
 class TestVariable(unittest.TestCase):
-    pass
+
+    def test_create_with_non_alphabetic_character_fails(self):
+        self.assertRaises(TypeError, lambda l: Variable(1))
+
+    def test_create_sets_value(self):
+        v = Variable('a')
+        self.assertEqual('a', v.value)
